9d943af4541bc640dd30dd2ee962bd136897a1b6
micatovo <micaelatovo@gmail.com>
2022-06-06 20:04:29 -0300

    segundo proyecto

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
-let password = +(prompt("ingrese su contraseña"));
+//conocer el valor total del producto segun el interes por cuotas
 
-while (password != "123456"){
-  alert("contraseña incorrecta");
-    password = +(prompt("ingrese su contraseña"));
+let precioProducto = +(prompt("ingrese el precio del producto a comprar"));
+let cuotas = +(prompt("Desea realizarlo en 1, 3 o 6 cuotas?"));
+let resultado = 0;
+
+  
+if (cuotas==1){
+  resultado = precioProducto;
+  alert ("Su producto quedara en un total de: $ " + precioProducto);
+  continuar();
 }
+else if (cuotas==3){
+  resultado = (precioProducto * 1.1);
+  alert ("Su producto quedara en un total de: $ " + resultado);
+  continuar();
+}
+  else if (cuotas==6){
+    resultado = (precioProducto * 1.5);
+    alert ("Su producto quedara en un total de: $" + resultado);
+    continuar();
+  }
+  else {
+    alert ("error");
+  }
+
+function continuar (){
+  let continuar = +(prompt("Desea saber el valor de algun otro producto? 1-si / 2-no"));
+  if (continuar == 1){
+    alert ("presione f5");
+  } else {
+    alert ("puede cerrar esta ventana");
+  }
+  }
