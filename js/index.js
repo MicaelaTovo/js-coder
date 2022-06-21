72ce6e45a432ea0e6ee94e6360b6a5b2fcfd8909
micatovo <micaelatovo@gmail.com>
2022-06-20 21:05:59 -0300

    primera entrega proyecto final

--- js/index.js
+++ js/index.js
@@ -1,76 +1,62 @@
-const viandas = ['proteico', 'keto','gourmet'];
-
-for (let index = 0; index < viandas.length; index++){
-  alert("posicion " + index + " vianda " + viandas[index]);
-}
-
-viandas.unshift('veggie');
-
-console.log(viandas);
-
-class Veggie {
-  constructor (nombre, precio, stock){
-    this.nombre = nombre.toUpperCase();
+class Viandas{
+  constructor(nombre, precio){
+    this.nombre = nombre;
     this.precio = precio;
-    this.stock = stock; 
+  }
+  Valores(){
+    return ("El pack " + this.nombre + " tiene un valor de $ " + this.precio);
   }
 }
 
-const comidas = [
-  {nombre: "tarta", precio: 300,  stock:10},
-  {nombre: "falafel", precio: 200,  stock:8}
-]
-//asi seria para incoporar cada uno? o es con push o pusg (new veggie)??
+const vianda1 = new Viandas("veggie", 1500);
+const vianda2 = new Viandas("proteico", 1800);
+const vianda3 = new Viandas("keto", 1900);
+const vianda4 = new Viandas("gourmet", 2100);
 
-console.log(comidas);
+let packs = ['veggie', 'proteico', 'keto', 'gourmet'];
+let carrito = [];
+let compra = +(prompt("Desea comprar un pack de viandas?\n 1-SI\n 2-NO"));
+let entrada = undefined;
 
-comidas.push({nombre: "curry", precio: 250,  stock:11});
+function verViandas(){
+  entrada = +(prompt(`Marca el numero del pack a comprar: \n 
+  1-${vianda1.Valores()} \n 
+  2-${vianda2.Valores()} \n 
+  3-${vianda3.Valores()} \n 
+  4-${vianda4.Valores()}`));
 
-console.table(comidas);
+}
 
+if (compra===1){
+  verViandas();
+}else if (compra ===2){ 
+  alert ("Hasta Pronto!")
+} else{
+  alert("Opcion invalida");
+  +(prompt("Desea comprar un pack de viandas?\n 1-SI\n 2-NO"));
+  verViandas();
+}
 
+function agregarPack (){
+packs.forEach((producto, index)=>{
+  if(entrada==index){
+    carrito.push(producto);
+    console.log (carrito);
+  }
+ })
+}
 
-
-
-
-
-
-
-
-
-/*
-//conocer el valor total del producto segun el interes por cuotas
-
-let precioProducto = +(prompt("ingrese el precio del producto a comprar"));
-let cuotas = +(prompt("Desea realizarlo en 1, 3 o 6 cuotas?"));
-let resultado = 0;
+if (entrada <=4){
+  let agregar = +(prompt("Quiere agregar algun otro pack? \n 1-SI\n 2-NO"));
+  if (agregar ===1){
+    agregarPack();
+    verViandas();
+    agregarPack();
+    alert("Usted agrego los packs " + carrito);
+  }else{
+    agregarPack();
+    alert("Usted agrego el pack " + carrito);
+  }
+  }
 
   
-if (cuotas==1){
-  resultado = precioProducto;
-  alert ("Su producto quedara en un total de: $ " + precioProducto);
-  continuar();
-}
-else if (cuotas==3){
-  resultado = (precioProducto * 1.1);
-  alert ("Su producto quedara en un total de: $ " + resultado);
-  continuar();
-}
-  else if (cuotas==6){
-    resultado = (precioProducto * 1.5);
-    alert ("Su producto quedara en un total de: $" + resultado);
-    continuar();
-  }
-  else {
-    alert ("error");
-  }
-
-function continuar (){
-  let continuar = +(prompt("Desea saber el valor de algun otro producto? 1-si / 2-no"));
-  if (continuar == 1){
-    alert ("presione f5");
-  } else {
-    alert ("puede cerrar esta ventana");
-  }
-  }
-*/
