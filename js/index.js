c18b2fbcc3c4ad55358743ecc1e7a126a2fb43e1
micatovo <micaelatovo@gmail.com>
2022-06-28 23:37:55 -0300

    eventos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,43 @@
-class Viandas{
+let formulario = document.getElementById("formulario");
+formulario.addEventListener('submit', formularioEnviado);
+
+function formularioEnviado(e){
+  e.preventDefault();
+  alert ("El formulario ha sido enviado correctamente");
+}
+
+
+let campo1 = document.getElementById("nombre");
+campo1.onclick = () => {console.log ('hicieron click en el campo nombre')};
+
+let campo2 = document.getElementById("apellido");
+campo2.onkeydown = () => {console.log('tecla apretada en campo apellido')};
+
+
+let form = document.getElementById("fielset");
+let newInput = document.createElement("reset");
+newInput.innerHTML = `<input type="reset" value="Borrar">`;
+form.append(newInput);
+
+
+
+
+
+
+
+
+
+// trabajos anteriores / pruebas
+
+
+// let contenedor = document.getElementById("container");
+// let contenedor_selec = document.getElementById("container_selec");
+// let seleccionados = [];
+
+
+
+
+/*class Viandas{
   constructor(nombre, precio){
     this.nombre = nombre;
     this.precio = precio;
@@ -57,6 +96,4 @@ if (entrada <=4){
     agregarPack();
     alert("Usted agrego el pack " + carrito);
   }
-  }
-
-  
+  }*/
---
+++ js/viandas.js
@@ -0,0 +1,63 @@
+const comidas = [
+    {
+    id: 1,
+    first_name: "Waldemar",
+    foto: "https://robohash.org/dolorecumvero.png?size=50x50&set=set1",
+    vianda: "Bread - Bistro Sour"
+  }, {
+    "id": 2,
+    "first_name": "Grata",
+    "foto": "https://robohash.org/reprehenderitetad.png?size=50x50&set=set1",
+    "vianda": "Longos - Assorted Sandwich"
+  }, {
+    "id": 3,
+    "first_name": "Aurea",
+    "foto": "https://robohash.org/placeatnihilsunt.png?size=50x50&set=set1",
+    "vianda": "Apple - Fuji"
+  }, {
+    "id": 4,
+    "first_name": "Elberta",
+    "foto": "https://robohash.org/inventoreliberosint.png?size=50x50&set=set1",
+    "vianda": "Mince Meat - Filling"
+  }, {
+    "id": 5,
+    "first_name": "Lance",
+    "foto": "https://robohash.org/minimaautet.png?size=50x50&set=set1",
+    "vianda": "Flour - Fast / Rapid"
+  }, {
+    "id": 6,
+    "first_name": "Gusti",
+    "foto": "https://robohash.org/dolorestotamincidunt.png?size=50x50&set=set1",
+    "vianda": "Flavouring Vanilla Artificial"
+  }, {
+    "id": 7,
+    "first_name": "Daron",
+    "foto": "https://robohash.org/etnonvoluptatem.png?size=50x50&set=set1",
+    "vianda": "Olives - Morracan Dired"
+  }, {
+    "id": 8,
+    "first_name": "Verena",
+    "foto": "https://robohash.org/errorremut.png?size=50x50&set=set1",
+    "vianda": "Irish Cream - Baileys"
+  }, {
+    "id": 9,
+    "first_name": "Keen",
+    "foto": "https://robohash.org/sintnecessitatibusdolorem.png?size=50x50&set=set1",
+    "vianda": "Chicken Breast Halal"
+  }, {
+    "id": 10,
+    "first_name": "Joete",
+    "foto": "https://robohash.org/vellaboreeaque.png?size=50x50&set=set1",
+    "vianda": "White Baguette"
+  }, {
+    "id": 11,
+    "first_name": "Carley",
+    "foto": "https://robohash.org/illoquiincidunt.png?size=50x50&set=set1",
+    "vianda": "Bok Choy - Baby"
+  }, {
+    "id": 12,
+    "first_name": "Hazel",
+    "foto": "https://robohash.org/pariatursitmagnam.png?size=50x50&set=set1",
+    "vianda": "Appetiser - Bought"
+  }]
+  
