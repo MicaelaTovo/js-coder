ad93bd29d3bb27b76708df4a60015862c4e66053
micatovo <micaelatovo@gmail.com>
2022-07-20 01:53:17 -0300

    proyecto final

--- js/index.js
+++ js/index.js
@@ -1,216 +1,143 @@
-let contenedor = document.getElementById("container");
-let contenedorSelect = document.getElementById("containerSelec");
-let seleccionados = []; 
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
 
 
-
-cargarSeleccionados();
-mostrarViandas();
-mostrarSeleccionados();
-filtroViandas();
-
-
-function cargarSeleccionados() {
-  localStorage.getItem("StorageSeleccionados") !== null ? seleccionados = JSON.parse(localStorage.getItem("StorageSeleccionados")) : localStorage.setItem("StorageSeleccionados", JSON.stringify(seleccionados))
-  }
+document.addEventListener('DOMContentLoaded', e => { 
+  fetchData()
+  if (localStorage.getItem('carrito')){
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    mostrarCarrito()}
+ });
 
 
-function mostrarViandas(){
-  fetch('./js/viandas.json')
-  .then ((resp) => resp.json())
-  .then ((viandas) => {
-    viandas.forEach((viandas)=>{
-      contenedor.innerHTML +=`
-    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-  <div class="card-header"> ${viandas.id} ${viandas.vianda} </div>
-  <div class="card-body">
-    <h5 class="card-title">${viandas.precio}</h5>
-    <p class="card-text">texto</p>
-    <button onClick="viandaSeleccionada(${viandas.id})" class="btn btn-outline-success">Quiero!</button>
-  </div>
-</div>`
+cards.addEventListener('click', e => { agregarCarrito(e) });
+items.addEventListener('click', e => { btnAumentarDisminuir(e) })
+
+
+const fetchData = async () => {
+    const res = await fetch('./js/viandas.json');
+    const data = await res.json()
+    mostrarViandas(data)
+}
+
+const mostrarViandas = data => {
+    data.forEach(item => {
+        templateCard.querySelector('h5').textContent = item.title
+        templateCard.querySelector('p').textContent = item.precio
+        templateCard.querySelector('img').setAttribute("src", item.img)
+        templateCard.querySelector('button').dataset.id = item.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
     })
-  })
+    cards.appendChild(fragment)
 }
 
 
-/*
-function mostrarViandas() {
-  viandas.forEach((viandas) => {
-    contenedor.innerHTML +=`
-    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-  <div class="card-header"> ${viandas.id} ${viandas.vianda} </div>
-  <div class="card-body">
-    <h5 class="card-title">${viandas.precio}</h5>
-    <p class="card-text">texto</p>
-    <button onClick="viandaSeleccionada(${viandas.id})" class="btn btn-outline-success">Quiero!</button>
-  </div>
-</div>`
-  });
-}
-*/
-
-function filtroViandas() {
-  const entrada = document.querySelector(".buscarViandas");
-  entrada.addEventListener("keyup", (e) => {
-    console.log(e);
-
-    let entradaMinuscula = e.target.value.toLowerCase(); 
-    console.log(entradaMinuscula);
-    if (e.target.matches(".buscarViandas")) {
-      document.querySelectorAll(".card").forEach(
-        (el) =>
-          el.textContent.toLowerCase().includes(entradaMinuscula)? el.classList.remove("filtrar"): el.classList.add("filtrar")
-      );
+const agregarCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        setCarrito(e.target.parentElement) & swal({title: "Genial", text: "Tu vianda fue agregada al carrito!", icon: "success", button: "Ok",
+      })
     }
-  });
+    e.stopPropagation()
+}
+
+const setCarrito = item => {
+    const producto = {
+        title: item.querySelector('h5').textContent,
+        precio: item.querySelector('p').textContent,
+        id: item.querySelector('button').dataset.id,
+        cantidad: 1
+    }
+  if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+      carrito[producto.id] = { ...producto }
+      mostrarCarrito()
+}
+
+const mostrarCarrito = () => {
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+        
+        
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    mostrarFooter()
+    
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+
 }
 
 
-function mostrarSeleccionados () {
-  seleccionados.forEach((seleccionados) => {
-    contenedorSelect.innerHTML +=`
-<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-<div class="card-header"> ${seleccionados.id} ${seleccionados.vianda} </div>
-<div class="card-body">
-<h5 class="card-title">${seleccionados.precio}</h5>
-<p class="card-text">texto</p>
-<button id="btn2" onClick=" quitarSeleccionados (${seleccionados.id});" class="btn btn-outline-success">No quiero!</button>
-</div>
-</div>`
-}
-);
-}
+const mostrarFooter = () => {
+    footer.innerHTML = ''
+    
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Su carrito se encuentra vacío </th>
+        `
+        return
+    }
+    
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
 
-  
-function viandaSeleccionada (identificador) {
-  let indice = identificador - 1;
-  let viandaElegida = {};
-  viandaElegida = viandas[indice];
-  !seleccionados.some(e=>e.id===identificador)? seleccionados.push(viandaElegida)  & localStorage.setItem("StorageSeleccionados", JSON.stringify(seleccionados)) & location.reload() : swal({title: "Upsss", text: "Vianda ya cargada!", icon: "error", button: "Ok",
-  });;
-}
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
 
-function quitarSeleccionados(id) {
-  let seleccionadosFiltrado = seleccionados.filter((elemento) => elemento.id != id);
-  seleccionados = seleccionadosFiltrado;
-  localStorage.setItem("StorageSeleccionados", JSON.stringify(seleccionados));
-  Toastify({
-    text: "Eliminado correctamente",
-    duration: 3000,
-    gravity: 'bottom',
-    position: 'left',})
-    .showToast();
-  location.reload()
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
 
-}
-  
- 
+    footer.appendChild(fragment)
 
-
-
-
-
-
-
-
-
-// let formulario = document.getElementById("formulario");
-// formulario.addEventListener('submit', formularioEnviado);
-
-// function formularioEnviado(e){
-//   e.preventDefault();
-//   alert ("El formulario ha sido enviado correctamente");
-// }
-
-
-// let campo1 = document.getElementById("nombre");
-// campo1.onclick = () => {console.log ('hicieron click en el campo nombre')};
-
-// let campo2 = document.getElementById("apellido");
-// campo2.onkeydown = () => {console.log('tecla apretada en campo apellido')};
-
-
-// let form = document.getElementById("fielset");
-// let newInput = document.createElement("reset");
-// newInput.innerHTML = `<input type="reset" value="Borrar">`;
-// form.append(newInput);
-
-
-
-
-
-
-
-
-
-// trabajos anteriores / pruebas
-
-
-// let contenedor = document.getElementById("container");
-// let contenedor_selec = document.getElementById("container_selec");
-// let seleccionados = [];
-
-
-
-
-/*class Viandas{
-  constructor(nombre, precio){
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-  Valores(){
-    return ("El pack " + this.nombre + " tiene un valor de $ " + this.precio);
-  }
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        carrito = {}
+        swal({title: "Upsss", text: "Acabas de vaciar tu carrito!", icon: "error", button: "Ok",
+      })
+        mostrarCarrito()
+    })
 }
 
-const vianda1 = new Viandas("veggie", 1500);
-const vianda2 = new Viandas("proteico", 1800);
-const vianda3 = new Viandas("keto", 1900);
-const vianda4 = new Viandas("gourmet", 2100);
+const btnAumentarDisminuir = e => {
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = { ...producto }
+        mostrarCarrito()
+    }
 
-let packs = ['veggie', 'proteico', 'keto', 'gourmet'];
-let carrito = [];
-let compra = +(prompt("Desea comprar un pack de viandas?\n 1-SI\n 2-NO"));
-let entrada = undefined;
-
-function verViandas(){
-  entrada = +(prompt(`Marca el numero del pack a comprar: \n 
-  1-${vianda1.Valores()} \n 
-  2-${vianda2.Valores()} \n 
-  3-${vianda3.Valores()} \n 
-  4-${vianda4.Valores()}`));
-
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        } else {
+            carrito[e.target.dataset.id] = {...producto}
+            Toastify({
+              text: "Eliminado correctamente",
+              duration: 3000,
+              gravity: 'bottom',
+              position: 'left',})
+              .showToast();
+        }
+        mostrarCarrito()
+    }
+    e.stopPropagation()
 }
-
-if (compra===1){
-  verViandas();
-}else if (compra ===2){ 
-  alert ("Hasta Pronto!")
-} else{
-  alert("Opcion invalida");
-  +(prompt("Desea comprar un pack de viandas?\n 1-SI\n 2-NO"));
-  verViandas();
-}
-
-function agregarPack (){
-packs.forEach((producto, index)=>{
-  if(entrada==index){
-    carrito.push(producto);
-    console.log (carrito);
-  }
- })
-}
-
-if (entrada <=4){
-  let agregar = +(prompt("Quiere agregar algun otro pack? \n 1-SI\n 2-NO"));
-  if (agregar ===1){
-    agregarPack();
-    verViandas();
-    agregarPack();
-    alert("Usted agrego los packs " + carrito);
-  }else{
-    agregarPack();
-    alert("Usted agrego el pack " + carrito);
-  }
-  }*/
